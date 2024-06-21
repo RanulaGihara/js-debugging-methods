d2455202393f31c308da3f4375897c2b52f62449
ranulagihara <ranulaw@opusxenta.com>
2024-06-21 12:43:49 +0530

    table

---
+++ table.js
@@ -0,0 +1,7 @@
+const people = [
+        { name: "John", age: 25, city: "New York" },
+        { name: "Jane", age: 30, city: "Paris" },
+        { name: "Bob", age: 35, city: "London" },
+    ];
+    
+    console.table(people);
