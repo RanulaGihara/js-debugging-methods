cded6514a651f740533b1ed59521bb6352f5e0ee
ranulagihara <ranulaw@opusxenta.com>
2024-06-21 12:39:21 +0530

    assertions

---
+++ assertion.js
@@ -0,0 +1,9 @@
+const X = 1;
+const Y = 2;
+const Z = 3;
+
+console.assert(X === 1, "X should be 1");
+console.assert(Y === 2, "Y should be 2");
+console.assert(Z === 3, "Z should be 3");
+
+console.log("All assertions passed"); 
